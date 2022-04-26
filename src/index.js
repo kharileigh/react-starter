250834efde56e5792aab3476b2b6d2fe985bd157
Peritract <dan@getfutureproof.co.uk>
2022-04-26 12:43:40 +0100

    Add functional component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import reactDom from 'react-dom';
 
-reactDom.render(<h1>Bees</h1>, document.querySelector("root"));
+function App () {
+    return <h1>Hello, World!</h1>
+}
+
+reactDom.render(<App />, document.querySelector("root"));
